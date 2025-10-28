3ffb7339f9f6735a3ac686b82e00612f6416e4cd
YvesLangstonMays <yveslangstonmays@gmail.com>
2025-10-28 16:20:40 -0500

    upd

--- main.py
+++ main.py
@@ -1,11 +1,11 @@
 import nflreadpy as nfl
 
-seanp_teams = ["NYG", "PHI", "DAL", "NO"]  # use team abbreviations
+seanp_teams = ["NYG", "PHI", "DAL", "NO"]
 
-# Load player stats (regular season)
+
 qb_stats = nfl.load_pbp_data("player_stats")
 
-# Filter for QBs and Sean Payton teams
+
 peyton_qbs = qb_stats[
     (qb_stats["position"] == "QB") & (qb_stats["team"].isin(seanp_teams))
 ]
